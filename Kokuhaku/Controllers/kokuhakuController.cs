f7f46ee1c1c08315baee169cf21261155ee37d9b
松尾 <n-matsuo@future-n.co.jp>
2016-03-16 10:26:27 +0900

    初めてのコミット

---
+++ Kokuhaku/Controllers/kokuhakuController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Kokuhaku.Models;
+
+namespace Kokuhaku.Controllers
+{
+    public class kokuhakuController : Controller
+    {
+        private kokuhakuContext db = new kokuhakuContext();
+        // GET: kokuhaku
+        public ActionResult Index()
+        {
+            return View(db.kokuhakus);
+        }
+    }
+}
---
+++ Kokuhaku/Models/kokuhaku.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kokuhaku.Models
+{
+    public class kokuhaku
+    { 
+    public int honki { get; set; }
+
+    }
+}
